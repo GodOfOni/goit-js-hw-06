e3d4f5683f77f2344cf8edbc5b2506b1a1ed49ac
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2023-09-24 12:11:07 +0200

    Delete src directory

--- src/js/task-06.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("validation-input");
-
-input.addEventListener("blur", (e) => {
-  e.target.value.length === +input.dataset.length
-    ? (input.className = "valid")
-    : (input.className = "invalid");
-});
-console.log(input.dataset.length);
--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById("font-size-control");
-const output = document.getElementById("text");
-
-input.addEventListener("input", (e) => {
-  output.style.fontSize = e.currentTarget.value + "px";
-});
