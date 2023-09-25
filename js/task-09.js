bc1d8729a3a7997944e156a6def02c9a7a52b325
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2023-09-25 19:03:30 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,11 @@
+const changeColorBtn = document.querySelector(".change-color");
+const displayHEX = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const btn = document.querySelector(".change-color");
-const colorEl = document.querySelector(".color");
 
 changeColorBtn.addEventListener("click", () => {
   displayHEX.textContent = getRandomHexColor();
