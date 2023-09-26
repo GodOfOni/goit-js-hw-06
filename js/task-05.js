a12c90f9248369389e448bc10cc171d78e819ae0
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2023-09-26 18:36:51 +0200

    Delete js directory

--- js/task-05.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.getElementById("name-input");
-const outputEl = document.getElementById("name-output");
-
-inputEl.addEventListener("input", () => {
-  inputEl.value === ""
-    ? (outputEl.innerHTML = "Anonymous")
-    : (outputEl.innerHTML = inputEl.value);
-});
